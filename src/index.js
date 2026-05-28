20523f49ca2fc00cf3bc8a62a06b5682c96c4eda
RECV <134547520+RECV00@users.noreply.github.com>
2026-05-28 17:02:17 -0600

    Add routes

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
 // Ruta de prueba
-app.get('/', (req, res) => {
-  res.json({ message: 'API Sistema Academico funcionando' });
-});
+//app.get('/', (req, res) => {
+//  res.json({ message: 'API Sistema Academico funcionando' });
+//});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
