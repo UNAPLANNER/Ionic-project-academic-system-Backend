f55541805a3e05e4f707917946017d1c8a293946
RECV <134547520+RECV00@users.noreply.github.com>
2026-05-28 19:48:08 -0600

    add data models for student, course and evaluation

---
+++ src/models/student.model.js
@@ -0,0 +1,11 @@
+const studentSchema = {
+  id: String,
+  name: String,
+  email: String,
+  career: String,
+  semester: Number,
+  photoUrl: String,
+  createdAt: Date
+};
+
+module.exports = studentSchema;
