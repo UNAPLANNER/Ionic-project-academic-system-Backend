4f81221793ccd6fcc0cd20b31ad37d338f75b76a
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-06-02 18:08:46 -0600

    Update courses.routes.js y students.routes.js

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllStudents, createStudent, updateStudent } = require('../controllers/students.controller');
+const { getStudents, getStudentById, createStudent, updateStudent } = require('../controllers/students.controller');
 const { verifyToken, checkRole } = require('../middleware/auth.middleware');
 
-router.get('/',     verifyToken, checkRole('teacher', 'admin'), getAllStudents);
+router.get('/',     verifyToken, checkRole('teacher', 'admin'), getStudents);
+router.get('/:id',  verifyToken, checkRole('teacher', 'admin'), getStudentById);
 router.post('/',    verifyToken, checkRole('teacher', 'admin'), createStudent);
 router.put('/:id',  verifyToken, checkRole('teacher', 'admin'), updateStudent);
 
