87aa3fc416eb2b34ec1670371161ccb4dcdb6fcc
adrianaOM3 <111209153+adrianaOM3@users.noreply.github.com>
2026-06-11 02:28:46 -0600

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,37 @@
+import js from "@eslint/js";
+import jestPlugin from "eslint-plugin-jest";
+
 export default [
   {
+    files: ["**/*.js"],
     languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
       globals: {
         require: "readonly",
         module: "readonly",
         exports: "readonly",
         process: "readonly",
-        console: "readonly"
+        console: "readonly",
+        fetch: "readonly"
       }
+    }
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ["tests/**/*.js"],
+    plugins: {
+      jest: jestPlugin
+    },
+    languageOptions: {
+      globals: {
+        ...jestPlugin.environments.globals.globals
+      }
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules
     }
   }
 ];
